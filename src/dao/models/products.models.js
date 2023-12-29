7d755b7e147c7b652a5fc899a5d69cfb16becb96
iancaceres <iaancaceress1@gmail.com>
2023-12-28 23:49:47 -0300

    BACKEND-SEGUNDA-PRE

---
+++ src/dao/models/products.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2"
+
+const productSchema = new mongoose.Schema({
+
+    title:{
+        type: String,
+        require: true
+    },
+    description:{
+        type: String,
+        require: true
+    },
+    code:{
+        type: Number,
+        require: true
+    },
+    price:{
+        type: String,
+        require: true
+    },
+    stock:{
+        type: Number,
+        require: true
+    },
+    category:{
+        type: String,
+        require: true
+    },
+    status: true,
+
+    thumbnails:{
+        type: Array,
+        require: true
+    },
+    
+})
+
+productSchema.plugin(mongoosePaginate)
+
+const productModel = mongoose.model("products",productSchema)
+
+export default productModel
